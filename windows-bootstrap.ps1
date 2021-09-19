2889755c662efaffacaae53ef9dd6d041be8b8a2
neko3cs <minaton.0722.net@gmail.com>
2021-09-19 23:10:10 +0900

    自動実行出来るスクリプトを記述

--- windows-bootstrap.ps1
+++ windows-bootstrap.ps1
@@ -1,33 +1,31 @@
-# Windows PowerShell
+#!Windows PowerShell
 
-# Check for run as administrator
 [Security.Principal.WindowsPrincipal]$CurrentPrincipal = [Security.Principal.WindowsIdentity]::GetCurrent()
 if (-not($CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
     Write-Output "It must be run as administrator!"
     exit
 }
-# Set Execution Polycy
+
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-# Install Chocolatey
+
 if (-not (Get-Command choco -ea SilentlyContinue)) {
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-# Windows is not default installed git
+
 if (-not (Get-Command git -ea SilentlyContinue)) {
     choco install git.install --yes --params="'/NoShellIntegration /NoAutoCrlf'"
 }
-# Clone .dotfiles
+
 if (!(Test-Path $HOME\.dotfiles)) {
     git clone https://github.com/neko3cs/.dotfiles.git
     Set-Location .dotfiles
 }
-# Encourage run PSScripts
-Write-Host "Please run this script!"
-Get-ChildItem -Filter "Set-*" | 
-ForEach-Object {
-    Write-Output "- $($_.Name)" 
-}
-Get-ChildItem -Filter "Install-*" | 
-ForEach-Object {
-    Write-Output "- $($_.Name)" 
-}
+
+. .\Set-PwshProfile.ps1
+. .\Set-VimConfigs.ps1
+. .\Set-WindowsOptionalFeature.ps1
+. .\Install-SQLServerManagementStudio.ps1
+
+# 以下のスクリプトはオプション指定が必要なため手動で入れる
+# - Install-ChocolateyApps.ps1
+# - Install-VisualStudio.ps1
