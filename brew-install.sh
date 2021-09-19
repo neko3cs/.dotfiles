3ad376936400c4978e8f15bb41561aac945f1146
neko3cs <minaton.0722.net@gmail.com>
2021-09-19 23:21:21 +0900

    brew_install の前にパスを通す必要がある

--- brew-install.sh
+++ brew-install.sh
@@ -4,6 +4,13 @@ type brew >/dev/null 2>&1 || {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
+if [ "$(uname -s)" == "Linux" ]; then
+  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bash_profile
+  echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.profile
+fi
+
 brew doctor
 brew update
 brew upgrade
