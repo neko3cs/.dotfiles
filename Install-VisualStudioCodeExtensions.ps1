45ac32a41aa22033da8bf7e723469bd36d96cd1e
neko3cs <minaton.0722.net@gmail.com>
2021-09-19 13:05:12 +0900

    add vscode extensions

--- Install-VisualStudioCodeExtensions.ps1
+++ Install-VisualStudioCodeExtensions.ps1
@@ -3,13 +3,15 @@
 # Execute this command and copy & paste to array initialization.
 # PS> code --list-extensions | Select-Object @{Name="Extensions";Expression={ "`"$_`"" }}
 $extensions = @(
+    "ajhyndman.jslint"
     "Angular.ng-template"
     "chrmarti.regex"
+    "DavidAnson.vscode-markdownlint"
     "eamodio.gitlens"
     "eightHundreds.vscode-drawio"
-    "esbenp.prettier-vscode"
     "Gruntfuggly.todo-tree"
     "humao.rest-client"
+    "ICS.japanese-proofreading"
     "jebbs.plantuml"
     "jtlowe.vscode-icon-theme"
     "ms-azuretools.vscode-docker"
@@ -19,6 +21,7 @@ $extensions = @(
     "ms-python.python"
     "ms-python.vscode-pylance"
     "ms-toolsai.jupyter"
+    "ms-toolsai.jupyter-keymap"
     "ms-vscode-remote.remote-containers"
     "ms-vscode-remote.remote-wsl"
     "ms-vscode.azure-account"
@@ -27,6 +30,7 @@ $extensions = @(
     "ms-vscode.cpptools"
     "ms-vscode.powershell"
     "ms-vsliveshare.vsliveshare"
+    "msjsdiag.vscode-react-native"
     "oderwat.indent-rainbow"
     "ritwickdey.LiveServer"
     "techer.open-in-browser"
