c1a7881e0bd182957d8c0b7b7f3f134772c6c6cd
neko3cs <minaton.0722.net@gmail.com>
2021-09-24 19:24:14 +0900

    tab space: 4 -> 2

--- Set-PwshProfile.ps1
+++ Set-PwshProfile.ps1
@@ -1,9 +1,17 @@
 #!/usr/bin/env pwsh
 
 if (Test-Path $PROFILE) {
-    Remove-Item -Force $PROFILE
+	Remove-Item `
+		-Path $PROFILE `
+		-Force
 }
 
 # Can not 'New-Item -ItemType SymbolicLink'; it's specified file, so copy this.
-New-Item -ItemType File -Path $PROFILE -Force
-Copy-Item .\Microsoft.PowerShell_profile.ps1 $PROFILE -Force
+New-Item `
+	-ItemType File `
+	-Path $PROFILE `
+	-Force
+Copy-Item `
+	-Path .\Microsoft.PowerShell_profile.ps1 `
+	-Destination $PROFILE `
+	-Force
