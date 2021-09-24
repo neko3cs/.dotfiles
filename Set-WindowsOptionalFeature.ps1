8b325bd27f847fbbb60292feeb7e8d8ad025e309
neko3cs <minaton.0722.net@gmail.com>
2021-09-24 19:24:35 +0900

    wsl2 ubuntu auto settings

--- Set-WindowsOptionalFeature.ps1
+++ Set-WindowsOptionalFeature.ps1
@@ -3,16 +3,32 @@
 $OptionalFeatures = @(
   "Microsoft-Hyper-V-All"
   "Microsoft-Windows-Subsystem-Linux"
+  "VirtualMachinePlatform"
   "IIS-WebServerRole"
 )
 
+$EnabledFeature = $false
 foreach ($Feature in $OptionalFeatures) {
-  $State = (Get-WindowsOptionalFeature -Online -FeatureName $Feature).State
+  $State = 
+  Get-WindowsOptionalFeature `
+    -Online `
+    -FeatureName $Feature |
+  Select-Object `
+    -ExpandProperty State
+
   if ($State -eq "Enabled") {
     Write-Output "'$($Feature)' has already enabled."
     continue
   }
 
-  Enable-WindowsOptionalFeature -Online -FeatureName $Feature
+  Enable-WindowsOptionalFeature `
+    -Online `
+    -FeatureName $Feature `
+    -NoRestart
   Write-Output "'$($Feature)' is now enabled."
+  $EnabledFeature = $true
+}
+
+if ($EnabledFeature) {
+  Write-Output "Please restart OS..."
 }
--- windows-bootstrap.ps1
+++ windows-bootstrap.ps1
@@ -1,30 +1,38 @@
-#!/usr/bin/env Windows PowerShell
+#!Windows PowerShell
+
+function Set-Wsl2Ubuntu {
+  wsl --install --distribution Ubuntu
+  wsl --set-default Ubuntu
+  wsl --set-default-version 2
+}
 
 [Security.Principal.WindowsPrincipal]$CurrentPrincipal = [Security.Principal.WindowsIdentity]::GetCurrent()
 if (-not($CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
-    Write-Output "It must be run as administrator!"
-    exit
+  Write-Output "It must be run as administrator!"
+  exit
 }
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
 if (-not (Get-Command choco -ea SilentlyContinue)) {
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 if (-not (Get-Command git -ea SilentlyContinue)) {
-    choco install git.install --yes --params="'/NoShellIntegration /NoAutoCrlf'"
+  choco install git.install --yes --params="'/NoShellIntegration /NoAutoCrlf'"
 }
 
 if (!(Test-Path $HOME\.dotfiles)) {
-    git clone https://github.com/neko3cs/.dotfiles.git
-    Set-Location .dotfiles
+  git clone https://github.com/neko3cs/.dotfiles.git
+  Set-Location -Path .dotfiles
 }
 
 . .\Set-PwshProfile.ps1
 . .\Set-VimConfigs.ps1
 . .\Set-WindowsOptionalFeature.ps1
 . .\Install-SQLServerManagementStudio.ps1
+
+Set-Wsl2Ubuntu
 
 Write-Output @"
 Please run this script...
