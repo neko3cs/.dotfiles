eac4dbbe7acb8ad460dcdd50eba58242438b817b
neko3cs <minaton.0722.net@gmail.com>
2021-10-01 00:59:36 +0900

    neobundle -> vim-plug

---
+++ Install-VimPlug.ps1
@@ -0,0 +1,19 @@
+#!/usr/bin/env pwsh
+
+Invoke-WebRequest `
+  -UseBasicParsing `
+  -Uri https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |
+New-Item `
+  -Path $HOME/vimfiles/autoload/plug.vim `
+  -Force
+
+if (Test-Path $HOME/.vim/bundle/) {
+  mkdir -Path $HOME/.vim/bundle/
+}
+git clone https://github.com/tomasiser/vim-code-dark.git $HOME/.vim/bundle/vim-code-dark.git
+if (Test-Path $HOME/.vim/colors/codedark.vim) {
+  mkdir -Path $HOME/.vim/colors/
+}
+Copy-Item `
+  -Path $HOME/.vim/bundle/vim-code-dark.git/colors/codedark.vim `
+  -Destination $HOME/.vim/colors/codedark.vim
