3814762a8180985b0a9af72974f8790efbff30bb
neko3cs <minaton.0722.net@gmail.com>
2022-01-18 15:52:10 +0900

    処理の整理

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,8 +8,46 @@ $PowerShellModules = @(
     "ClassExplorer"
     "ImportExcel"
 )
+function Register-PowerShellModule {
+    foreach ($module in $PowerShellModules) {
+        if (-not (Get-Module -ListAvailable -Name $module)) {
+            Install-Module -Name $module
+        }
+        if (-not (Get-Module -Name $module)) {
+            Import-Module -Name $module
+        }
+    }
+    Set-PoshPrompt -Theme stelbent.minimal # oh-my-posh settings
+}
+function Register-WingetCompletion {
+    if (Get-Command winget -ea SilentlyContinue) {
+        Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+            param($wordToComplete, $commandAst, $cursorPosition)
+            [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+            $Local:word = $wordToComplete.Replace('"', '""')
+            $Local:ast = $commandAst.ToString().Replace('"', '""')
+            winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+        }
+    }
+}
+function Register-DotNetCompletion {
+    if (Get-Command dotnet -ea SilentlyContinue) {
+        Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+            param($commandName, $wordToComplete, $cursorPosition)
+            dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+        }
+    }
+}
 
 if ($IsWindows) {
+    Register-PowerShellModule
+    Register-WingetCompletion
+    Register-DotNetCompletion
+
     # Alias
     Set-Alias -Name open -Value 'C:\Windows\explorer.exe'
     Set-Alias -Name winmerge -Value "C:\Program Files (x86)\WinMerge\WinMergeU.exe"
@@ -23,38 +61,5 @@ if ($IsWindows) {
             [parameter(Mandatory, ValueFromPipeline)][string]$command
         )
         return (Get-Command -Name $command -ShowCommandInfo).Definition
-    }
-
-    # PowerShell Module
-    foreach ($module in $PowerShellModules) {
-        if (-not (Get-Module -ListAvailable -Name $module)) {
-            Install-Module -Name $module
-        }
-        if (-not (Get-Module -Name $module)) {
-            Import-Module -Name $module
-        }
-    }
-    Set-PoshPrompt -Theme stelbent.minimal # oh-my-posh settings
-
-    # winget completion
-    if (Get-Command winget -ea SilentlyContinue) {
-        Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-            param($wordToComplete, $commandAst, $cursorPosition)
-            [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-            $Local:word = $wordToComplete.Replace('"', '""')
-            $Local:ast = $commandAst.ToString().Replace('"', '""')
-            winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-            }
-        }
-    }
-    # dotnet completion
-    if (Get-Command dotnet -ea SilentlyContinue) {
-        Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-            param($commandName, $wordToComplete, $cursorPosition)
-            dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-            }
-        }
     }
 }
