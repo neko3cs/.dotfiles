a75ec757b7ff55ff90658b9bd82318fb1239e0f8
neko3cs <minaton.0722.net@gmail.com>
2024-03-08 11:34:44 +0900

    feature: linuxbrewをやめる

--- brew-install.sh
+++ brew-install.sh
@@ -4,13 +4,6 @@ type brew >/dev/null 2>&1 || {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >/dev/null
 }
 
-if [ "$(uname -s)" = "Linux" ]; then
-  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bash_profile
-  echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.profile
-fi
-
 echo "🍺 tap homebrew/bundle..."
 brew tap homebrew/bundle >/dev/null
 
