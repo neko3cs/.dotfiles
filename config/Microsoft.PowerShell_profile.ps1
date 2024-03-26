5111c57a959af4a7db8db5fff2be87e4d777c682
neko3cs <minaton.0722.net@gmail.com>
2024-03-26 12:00:40 +0900

    Azure CLIのタブ補完実装

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -41,6 +41,25 @@ function Register-DotNetCompletion {
         }
     }
 }
+function Register-AzureCliCompletion {
+    Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+        $completion_file = New-TemporaryFile
+        $env:ARGCOMPLETE_USE_TEMPFILES = 1
+        $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+        $env:COMP_LINE = $wordToComplete
+        $env:COMP_POINT = $cursorPosition
+        $env:_ARGCOMPLETE = 1
+        $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+        $env:_ARGCOMPLETE_IFS = "`n"
+        $env:_ARGCOMPLETE_SHELL = 'powershell'
+        az 2>&1 | Out-Null
+        Get-Content $completion_file | Sort-Object | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+        }
+        Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+    }
+}
 function Register-OhMyPoshSettings {
     oh-my-posh init pwsh `
         --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/stelbent.minimal.omp.json' | 
@@ -73,6 +92,7 @@ if ($IsWindows) {
 Register-PowerShellModule
 Register-OhMyPoshSettings
 Register-DotNetCompletion
+Register-AzureCliCompletion
 
 # PowerShell Customize
 Set-PSReadLineOption `
