db6d519471772bf4bc2d2b3c520471a5e3a911b2
neko3cs <minaton.0722.net@gmail.com>
2024-05-05 16:06:35 +0900

    PowerShellのPROFILEを配置する設定

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -2,6 +2,8 @@
 
 ln -sf $(pwd)/config/.zshrc $HOME/.zshrc
 source ~/.zshrc
+mkdir -p $HOME/.config/powershell
+cp -f $(pwd)/config/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
 ln -sf $(pwd)/config/starship.toml $HOME/.starship/starship.toml
 ln -sf $(pwd)/config/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/config/.gitconfig $HOME/.gitconfig
