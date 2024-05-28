15097f27065a746712fef8c81516ff408a684498
neko3cs <minaton.0722.net@gmail.com>
2024-05-28 14:33:08 +0900

    PSPROFILEの整理
    Register系は外だし

--- Set-DotFiles.ps1
+++ Set-DotFiles.ps1
@@ -1,11 +1,11 @@
 #Requires -RunAsAdministrator
 
 if ($IsWindows) {
-  New-Item -ItemType File -Force -Path $PROFILE
-  Copy-Item -Force -Path $PWD\config\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE
-  New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Value $PWD/config/.gitconfig
-  New-Item -ItemType SymbolicLink -Force -Path $HOME/.starship/starship.toml -Value $PWD/config/starship.toml
+  New-Item -ItemType File -Force -Path $PROFILE | Out-Null
+  Copy-Item -Force -Path $PWD\config\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE | Out-Null
+  New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Value $PWD/config/.gitconfig | Out-Null
+  New-Item -ItemType SymbolicLink -Force -Path $HOME/.starship/starship.toml -Value $PWD/config/starship.toml | Out-Null
 }
 elseif ($IsMacOS) {
-  Copy-Item -Force -Path ./config/Microsoft.PowerShell_profile.ps1 -Destination $PROFILE
+  Copy-Item -Force -Path ./config/Microsoft.PowerShell_profile.ps1 -Destination $PROFILE | Out-Null
 }
