f7c1254b0275a797edea7dee646481d4be34bb46
neko3cs <minaton.0722.net@gmail.com>
2024-06-01 21:56:33 +0900

    Rio Terminalの導入

--- Set-DotFiles.ps1
+++ Set-DotFiles.ps1
@@ -3,6 +3,7 @@
 if ($IsWindows) {
   New-Item -ItemType File -Force -Path $PROFILE | Out-Null
   Copy-Item -Force -Path $PWD\config\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE | Out-Null
+  New-Item -ItemType SymbolicLink -Force -Path $HOME/AppData/Local/rio/config.toml -Value $PWD/config/rio_config.toml | Out-Null
   New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Value $PWD/config/.gitconfig | Out-Null
   New-Item -ItemType SymbolicLink -Force -Path $HOME/.starship/starship.toml -Value $PWD/config/starship.toml | Out-Null
 }
