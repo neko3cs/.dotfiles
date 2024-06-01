80323983716a8ce299c409feac01bc815e59a0b1
neko3cs <minaton.0722.net@gmail.com>
2024-06-01 21:56:33 +0900

    Rio Terminalの導入

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -5,6 +5,7 @@ source ~/.zshrc
 mkdir -p $HOME/.config/powershell
 cp -f $(pwd)/config/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
 ln -sf $(pwd)/config/starship.toml $HOME/.starship/starship.toml
+ln -sf $(pwd)/config/rio_config.toml $HOME/.config/rio/config.toml
 ln -sf $(pwd)/config/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/config/.gitconfig $HOME/.gitconfig
 ln -sf $(pwd)/config/Brewfile $HOME/Brewfile
