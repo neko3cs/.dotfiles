8e7dcecb1f8f3ec63201622b3c7c2febe5966538
neko3cs <minaton.0722.net@gmail.com>
2024-08-06 15:29:54 +0900

    Windowsでのvimの導入

--- Set-DotFiles.ps1
+++ Set-DotFiles.ps1
@@ -6,6 +6,7 @@ if ($IsWindows) {
   New-Item -ItemType SymbolicLink -Force -Path $HOME/AppData/Local/rio/config.toml -Value $PWD/config/rio_config.toml | Out-Null
   New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Value $PWD/config/.gitconfig | Out-Null
   New-Item -ItemType SymbolicLink -Force -Path $HOME/.starship/starship.toml -Value $PWD/config/starship.toml | Out-Null
+  New-Item -ItemType SymbolicLink -Force -Path $HOME/vimfiles/vimrc -Value $PWD/config/.vimrc | Out-Null
 }
 elseif ($IsMacOS) {
   Copy-Item -Force -Path ./config/Microsoft.PowerShell_profile.ps1 -Destination $PROFILE | Out-Null
