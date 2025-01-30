b2536ea515e7e6b527f2e88e652c148c084cb86d
Suzuki Minato (鈴木 湊) <Minato.Suzuki@jp.ricoh.com>
2025-01-30 10:09:20 +0900

    ファイルをパッケージマネージャーごとに分離する

---
+++ install_pacman-packages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while IFS= read -r package; do
+  if [ -n "$package" ]; then
+    sudo pacman -S --noconfirm "$package" > /dev/null
+  fi
+done < "$HOME/.dotfiles/config/pacman-packages.txt"
