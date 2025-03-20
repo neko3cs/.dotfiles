a5dc18bf9f793df88a675c2f65b6c005065aa20d
neko3cs <minaton.0722.net@gmail.com>
2025-03-21 08:44:46 +0900

    不要なアプリケーションの削除

--- bootstrap_macOS.sh
+++ bootstrap_macOS.sh
@@ -6,10 +6,6 @@ install_docker_completion() {
     curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose >~/.zsh/completion/_docker-compose
   fi
 }
-install_rust() {
-  rustup-init --quiet -y
-  rustup component add rustfmt rust-analyzer
-}
 setup_java() {
   sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 }
@@ -25,6 +21,5 @@ softwareupdate --install
 ./brew-install.sh
 setup_java
 install_docker_completion
-install_rust
 ./vimplug-install.sh
 ./dotfiles-link.sh
