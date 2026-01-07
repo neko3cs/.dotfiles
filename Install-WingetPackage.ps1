d7c58e55a5280ea1fd15247319146c313fc1308e
neko3cs <minaton.0722.net@gmail.com>
2026-01-08 01:35:51 +0900

    パス壊しちゃってたので修正

--- Install-WingetPackage.ps1
+++ Install-WingetPackage.ps1
@@ -11,14 +11,16 @@ if (-not (Get-Module -Name powershell-yaml)) {
     Import-Module -Name powershell-yaml
 }
 
-$packages = Get-Content -Path .\config\winget-package.yaml | ConvertFrom-Yaml
+$packages = Get-Content -Path .\winget-package.yaml | ConvertFrom-Yaml
 foreach ($package in $packages) {
     if ($null -ne $packages.Options) {
         Write-Output "Install $($package.Id) with options: '$($package.Options)'"
         winget install --id $package.Id --override $package.Options `
             --silent --accept-package-agreements --accept-source-agreements
     }
-    Write-Output "Install $($package.Id)"
-    winget install --id $package.Id `
-        --silent --accept-package-agreements --accept-source-agreements
+    else {
+        Write-Output "Install $($package.Id)"
+        winget install --id $package.Id `
+            --silent --accept-package-agreements --accept-source-agreements
+    }
 }
