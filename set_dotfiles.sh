1620a1cff6fd3f4da7a0b7115f509ec8d910d371
neko3cs <minaton.0722.net@gmail.com>
2026-01-08 01:35:51 +0900

    パス壊しちゃってたので修正

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -2,8 +2,8 @@
 
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
 source ~/.zshrc
-mkdir -p $HOME/powershell
-cp -f $(pwd)/Microsoft.PowerShell_profile.ps1 $HOME/powershell/Microsoft.PowerShell_profile.ps1
+mkdir -p $HOME/.config/powershell
+cp -f $(pwd)/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
 ln -sf $(pwd)/starship.toml $HOME/.starship/starship.toml
 ln -sf $(pwd)/rio_config.toml $HOME/.config/rio/config.toml
 ln -sf $(pwd)/.vimrc $HOME/.vimrc
