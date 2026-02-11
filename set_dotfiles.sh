df8432975002e62e399d885523eedc2b1b8db2ea
neko3cs <minaton.0722.net@gmail.com>
2026-02-12 02:16:10 +0900

    refactor: add comment and mkdir .starship dir

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env zsh
 
+# .zshrc
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
 source ~/.zshrc
+# pwsh
 mkdir -p $HOME/.config/powershell
 cp -f $(pwd)/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
+# starship
+mkdir -p $HOME/.starship
 ln -sf $(pwd)/starship.toml $HOME/.starship/starship.toml
+# ghostty
 mkdir -p $HOME/.config/ghostty
 ln -sf $(pwd)/ghostty_config $HOME/.config/ghostty/config
+# .vimrc
 ln -sf $(pwd)/.vimrc $HOME/.vimrc
+# .gitconfig
 ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
+# Homebrew
 ln -sf $(pwd)/Brewfile $HOME/Brewfile
+
