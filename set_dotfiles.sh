4e4aae30387d8933f53992b2749919c4af022e8e
neko3cs <minaton.0722.net@gmail.com>
2026-02-11 19:37:53 +0900

    Add Ghostty configuration and update setup script

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -6,6 +6,8 @@ mkdir -p $HOME/.config/powershell
 cp -f $(pwd)/Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
 ln -sf $(pwd)/starship.toml $HOME/.starship/starship.toml
 ln -sf $(pwd)/rio_config.toml $HOME/.config/rio/config.toml
+mkdir -p $HOME/.config/ghostty
+ln -sf $(pwd)/ghostty_config $HOME/.config/ghostty/config
 ln -sf $(pwd)/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/.gitconfig $HOME/.gitconfig
 ln -sf $(pwd)/Brewfile $HOME/Brewfile
