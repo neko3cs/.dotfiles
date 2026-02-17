54d9945d2f55af393e3c5297683d3e9ac5cfb94d
neko3cs <minaton.0722.net@gmail.com>
2026-02-17 18:11:25 +0900

    fix(homebrew): resolve deprecation warnings and cleanup installation script

--- install_brewfomulas.sh
+++ install_brewfomulas.sh
@@ -4,20 +4,17 @@ type brew >/dev/null 2>&1 || {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >/dev/null
 }
 
-echo "🍺 tap homebrew/bundle..."
-brew tap homebrew/bundle >/dev/null
-
-echo "🍺 doctor..."
-brew doctor >/dev/null
-
 echo "🍺 update..."
 brew update >/dev/null
 
 echo "🍺 upgrade..."
 brew upgrade >/dev/null
 
+echo "🍺 bundle..."
+brew bundle --file=$(pwd)/Brewfile
+
 echo "🍺 cleanup..."
 brew cleanup >/dev/null
 
-echo "🍺 bundle..."
-brew bundle >/dev/null
+echo "🍺 doctor..."
+brew doctor >/dev/null
