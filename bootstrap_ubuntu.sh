d581f5782e49262520948eb845b8013cdd20439d
neko3cs <minaton.0722.net@gmail.com>
2026-02-26 15:13:53 +0900

    update: lazygitがaptから取得できるようになった

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -64,12 +64,6 @@ install_aws_sam_cli() {
   unzip ~/aws-sam-cli-linux-x86_64.zip -d ~/sam-installer
   sudo ~/sam-installer/install && rm -rf ~/sam-installer/
 }
-install_lazygit() {
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  tar xf lazygit.tar.gz lazygit
-  sudo install lazygit /usr/local/bin
-}
 
 cd $HOME
 if  [ ! -d ./.dotfiles ]; then
@@ -91,7 +85,6 @@ install_docker
 install_yq
 install_azure_cli
 install_aws_sam_cli
-install_lazygit
 ./install_vimplug.sh
 ./set_dotfiles.sh
 ./set_completions.sh
