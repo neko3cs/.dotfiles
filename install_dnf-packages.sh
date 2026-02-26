98424fa47008410be9c6c0bd1875d4a5ef6761d2
neko3cs <minaton.0722.net@gmail.com>
2026-02-27 08:55:53 +0900

    fix: groupinstallの個別化 and キャッシュ強制更新 and lazygit repo

--- install_dnf-packages.sh
+++ install_dnf-packages.sh
@@ -7,6 +7,12 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft-2025.asc
 sudo dnf install -y https://packages.microsoft.com/config/rhel/10/packages-microsoft-prod.rpm
 # zsh-completions Repository
 sudo dnf copr enable clarlok/zsh-users -y
+# lazygit Repository
+sudo dnf copr enable dejan/lazygit
+
+sudo dnf makecache
+
+sudo dnf groupinstall -y "Development Tools"
 
 PACKAGES=$(grep -vE '^#|^$' "$PACKAGES_FILE" | tr '\n' ' ')
 sudo dnf install -y $PACKAGES
