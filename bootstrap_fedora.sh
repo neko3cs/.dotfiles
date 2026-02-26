c961c18586ca6b26efd801a619769aa5b397aa80
neko3cs <minaton.0722.net@gmail.com>
2026-02-27 08:47:08 +0900

    fix: add -y and chmod

--- bootstrap_fedora.sh
+++ bootstrap_fedora.sh
@@ -26,10 +26,10 @@ install_aws_cli() {
   sudo ./aws/install
 }
 install_docker() {
-  sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate \
-                  docker-logrotate docker-selinux docker-engine-selinux docker-engine
+  sudo dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate \
+                    docker-logrotate docker-selinux docker-engine-selinux docker-engine
   sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
-  sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo usermod -aG docker $USER
   sudo systemctl enable --now docker
 }
