5cf4293f2f1a8fcaf0da483cc3616d06e593ee8a
neko3cs <minaton.0722.net@gmail.com>
2026-02-27 09:06:03 +0900

    fix: インストール失敗したのでバージョン下げる

--- install_dnf-packages.sh
+++ install_dnf-packages.sh
@@ -3,16 +3,15 @@
 PACKAGES_FILE="$(dirname "$0")/dnf-packages.txt"
 
 # Microsoft Repository
-sudo rpm --import https://packages.microsoft.com/keys/microsoft-2025.asc
-sudo dnf install -y https://packages.microsoft.com/config/rhel/10/packages-microsoft-prod.rpm
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo dnf install -y https://packages.microsoft.com/config/rhel/9/packages-microsoft-prod.rpm
 # zsh-completions Repository
-sudo dnf copr enable clarlok/zsh-users -y
+sudo dnf copr enable -y clarlok/zsh-users
 # lazygit Repository
-sudo dnf copr enable dejan/lazygit
+sudo dnf copr enable -y dejan/lazygit
 
-sudo dnf makecache
-
-sudo dnf groupinstall -y "Development Tools"
+sudo dnf clean all
+sudo dnf makecache -y
 
 PACKAGES=$(grep -vE '^#|^$' "$PACKAGES_FILE" | tr '\n' ' ')
-sudo dnf install -y $PACKAGES
+sudo dnf install -y $PACKAGES --skip-unavailable
