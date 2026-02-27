54a4068c021f5fcd055ca0bb37f508dbd4db8cef
neko3cs <minaton.0722.net@gmail.com>
2026-02-27 09:57:10 +0900

    fix: && の書き方だと終了コードが1になってset -eで止まってしまう

--- set_completions.sh
+++ set_completions.sh
@@ -8,15 +8,21 @@ if command -v docker-compose >/dev/null 2>&1; then
     "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose" \
     -o "$completion_dir/_docker-compose"
 fi
-command -v deno >/dev/null 2>&1 \
-  && deno completions zsh > "$completion_dir/_deno"
-command -v dotnet >/dev/null 2>&1 \
-  && dotnet completions script zsh > "$completion_dir/_dotnet"
-command -v minikube >/dev/null 2>&1 \
-  && minikube completion zsh > "$completion_dir/_minikube"
-command -v pip >/dev/null 2>&1 \
-  && pip completion --zsh > "$completion_dir/_pip"
-command -v starship >/dev/null 2>&1 \
-  && starship completions zsh > "$completion_dir/_starship"
-command -v sqlcmd >/dev/null 2>&1 \
-  && sqlcmd completion zsh > "$completion_dir/_sqlcmd"
+if command -v deno >/dev/null 2>&1; then
+  deno completions zsh > "$completion_dir/_deno"
+fi
+if command -v dotnet >/dev/null 2>&1; then
+  dotnet completions script zsh > "$completion_dir/_dotnet"
+fi
+if command -v minikube >/dev/null 2>&1; then
+  minikube completion zsh > "$completion_dir/_minikube"
+fi
+if command -v pip >/dev/null 2>&1; then
+  pip completion --zsh > "$completion_dir/_pip"
+fi
+if command -v starship >/dev/null 2>&1; then
+  starship completions zsh > "$completion_dir/_starship"
+fi
+if command -v sqlcmd >/dev/null 2>&1; then
+  sqlcmd completion zsh > "$completion_dir/_sqlcmd"
+fi
