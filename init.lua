c6262c14b7798ec6debb45150343b8a17fc76176
neko3cs <minaton.0722.net@gmail.com>
2026-02-27 13:37:31 +0900

    update: 色味が変わっちゃうので、airlineに戻す

--- init.lua
+++ init.lua
@@ -51,11 +51,8 @@ require("lazy").setup({
     priority = 1000,
     config = function() vim.cmd([[colorscheme codedark]]) end
   },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = { options = { theme = 'codedark' } }
-  },
+  { "vim-airline/vim-airline" },
+  { "vim-airline/vim-airline-themes" },
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -77,7 +74,47 @@ require("lazy").setup({
   "jiangmiao/auto-pairs",
   "carlsmedstad/vim-bicep",
 })
+-- Apply colorscheme after plugin definition
+if vim.fn.has("termguicolors") == 1 then
+  vim.opt.termguicolors = true
+end
+local ok, _ = pcall(vim.cmd, "colorscheme codedark")
+if not ok then
+  print("Colorscheme 'codedark' not found. Please run :Lazy")
+end
+vim.api.nvim_set_hl(0, "CursorLine", {
+  ctermbg = 236,
+  bg = "#2a2a2a",
+  bold = false,
+  italic = false,
+  underline = false
+})
+
+-- === 3. PLUGIN SPECIFIC SETTINGS ===
+-- vim-airline
+vim.g.airline_theme = 'codedark'
+vim.g['airline#extensions#tabline#enabled'] = 1
+vim.g['airline#extensions#tabline#fnamemod'] = ':t'
+vim.g['airline#extensions#tabline#show_buffers'] = 1
+vim.g['airline#extensions#tabline#show_splits'] = 0
+vim.g['airline#extensions#tabline#show_tabs'] = 1
+vim.g['airline#extensions#tabline#show_tab_nr'] = 0
+vim.g['airline#extensions#tabline#show_tab_type'] = 1
+vim.g['airline#extensions#tabline#show_close_button'] = 0
+vim.g['airline#extensions#hunks#non_zero_only'] = 1
+
+vim.g['airline#extensions#default#layout'] = {
+  { 'a', 'b', 'c' },
+  { 'z' }
+}
+vim.g.airline_section_c = '%t %M'
+local prefix = vim.g.airline_linecolumn_prefix or ''
+vim.g.airline_section_z = prefix .. '%3l:%-2v'
+
+-- telescope.nvim
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+
+-- nvim-tree
 vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { silent = true })
