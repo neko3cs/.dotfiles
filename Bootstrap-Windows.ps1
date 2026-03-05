4f108ccfd73de8c140c07024fe04ca0c6ffb9a81
neko3cs <minaton.0722.net@gmail.com>
2026-03-05 13:31:29 +0900

    update: delete Enable-WindowsOptionalFeature.ps1
    PCセットアップ時にしか実行されないスクリプトはBootstrap-Windows.ps1にまとめる

--- Bootstrap-Windows.ps1
+++ Bootstrap-Windows.ps1
@@ -1,5 +1,6 @@
 #Requires -RunAsAdministrator
 #Requires -Version 7.0
+#Requires -Module Dism
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
@@ -8,6 +9,39 @@ function Set-Wsl2Fedora {
   wsl --set-default FedoraLinux-43
   wsl --set-default-version 2
 }
+function Enable-WindowsOptionalFeature {
+  $OptionalFeatures = @(
+    "Microsoft-Hyper-V-All"
+    "Microsoft-Windows-Subsystem-Linux"
+    "VirtualMachinePlatform"
+  )
+  $RebootRequired = $false
+  foreach ($Feature in $OptionalFeatures) {
+    $info = Get-WindowsOptionalFeature -Online -FeatureName $Feature
+    if ($null -eq $info) {
+      Write-Warning "Feature '$Feature' was not found."
+      continue
+    }
+    switch ($info.State) {
+      'Enabled' {
+        Write-Host "'$Feature' is already enabled."
+      }
+      'EnablePending' {
+        Write-Host "'$Feature' is pending enable. Reboot required."
+        $RebootRequired = $true
+      }
+      default {
+        Write-Host "Enabling '$Feature'..."
+        Enable-WindowsOptionalFeature -Online -FeatureName $Feature -NoRestart
+        $RebootRequired = $true
+        Write-Host "'$Feature' has been enabled."
+      }
+    }
+  }
+  if ($RebootRequired) {
+    Write-Output "Please restart OS..."
+  }
+}
 
 try {
   Get-Command winget -ErrorAction Stop | Out-Null
@@ -15,7 +49,6 @@ try {
 catch {
   Write-Error 'winget is not installed. Please install it from Microsoft Store.'
 }
-
 try {
   Get-Command git -ErrorAction Stop | Out-Null
 }
@@ -30,12 +63,12 @@ if (-not (Test-Path $dotfilesPath)) {
 }
 Set-Location -Path $dotfilesPath
 
-& (Join-Path $PSScriptRoot 'Enable-WindowsOptionalFeature.ps1')
 & (Join-Path $PSScriptRoot 'Install-WingetPackage.ps1')
 & (Join-Path $PSScriptRoot 'Install-PnpmPackage.ps1')
 & (Join-Path $PSScriptRoot 'Set-DotFiles.ps1')
 & (Join-Path $PSScriptRoot 'Set-Completions.ps1')
 Set-Wsl2Fedora
+Enable-WindowsOptionalFeature
 
 if (Test-Path $PROFILE) {
   . $PROFILE
