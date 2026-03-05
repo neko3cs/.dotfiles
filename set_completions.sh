5016b3603e04fd4b0413476f119cc17840ee4748
neko3cs <minaton.0722.net@gmail.com>
2026-03-05 21:33:08 +0900

    refactor: 大文字に統一

--- set_completions.sh
+++ set_completions.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env zsh
 
-completion_dir="$HOME/.zsh/completion"
-mkdir -p "$completion_dir"
+COMPLETIONS_DIR="$HOME/.zsh/completion"
+mkdir -p "$COMPLETIONS_DIR"
 
 if command -v deno >/dev/null 2>&1; then
-  deno completions zsh > "$completion_dir/_deno"
+  deno completions zsh > "$COMPLETIONS_DIR/_deno"
 fi
 if command -v docker >/dev/null 2>&1; then
-  docker completion zsh > "$completion_dir/_docker"
+  docker completion zsh > "$COMPLETIONS_DIR/_docker"
 fi
 if command -v dotnet >/dev/null 2>&1; then
-  dotnet completions script zsh > "$completion_dir/_dotnet"
+  dotnet completions script zsh > "$COMPLETIONS_DIR/_dotnet"
 fi
 if command -v gh >/dev/null 2>&1; then
-  gh completion -s zsh > "$completion_dir/_gh"
+  gh completion -s zsh > "$COMPLETIONS_DIR/_gh"
 fi
 if command -v minikube >/dev/null 2>&1; then
-  minikube completion zsh > "$completion_dir/_minikube"
+  minikube completion zsh > "$COMPLETIONS_DIR/_minikube"
 fi
 if command -v pip >/dev/null 2>&1; then
-  pip completion --zsh > "$completion_dir/_pip"
+  pip completion --zsh > "$COMPLETIONS_DIR/_pip"
 fi
 if command -v starship >/dev/null 2>&1; then
-  starship completions zsh > "$completion_dir/_starship"
+  starship completions zsh > "$COMPLETIONS_DIR/_starship"
 fi
 if command -v sqlcmd >/dev/null 2>&1; then
-  sqlcmd completion zsh > "$completion_dir/_sqlcmd"
+  sqlcmd completion zsh > "$COMPLETIONS_DIR/_sqlcmd"
 fi
