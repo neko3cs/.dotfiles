3d526e554fde297056558c5de41696b2fbc38cdd
neko3cs <minaton.0722.net@gmail.com>
2026-03-07 01:14:58 +0900

    update: pnpm パッケージは手動で入れる

--- bootstrap_macOS.sh
+++ bootstrap_macOS.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env zsh
 set -e
 
+SCRIPT_ROOT="$(dirname "$0")"
+
 cd $HOME
 if  [ ! -d $HOME/.dotfiles ]; then
   git clone https://github.com/neko3cs/.dotfiles.git
 fi
-cd .dotfiles
-
-SCRIPT_ROOT="$(dirname "$0")"
+cd $HOME/.dotfiles
 
 softwareupdate --install
 
-. $SCRIPT_ROOT/install_brewformulas.sh
-. $SCRIPT_ROOT/install_pnpm-packages.sh
-. $SCRIPT_ROOT/set_dotfiles.sh
 . $SCRIPT_ROOT/set_macOS_defaults.sh
+. $SCRIPT_ROOT/install_brewformulas.sh
+. $SCRIPT_ROOT/set_dotfiles.sh
 . $SCRIPT_ROOT/set_completions.sh
 pwsh -File $SCRIPT_ROOT/Set-Completions.ps1
