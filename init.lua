c9473b0df30a3c303c83fe56cc83bac03acfbb15
neko3cs <minaton.0722.net@gmail.com>
2026-03-22 12:27:15 +0900

    toggletermの追加

--- init.lua
+++ init.lua
@@ -163,6 +163,23 @@ require("lazy").setup({
     end
   },
 
+  -- ターミナルの統合
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        float_opts = {
+          border = "curved",
+          width = math.floor(vim.o.columns * 0.8),
+          height = math.floor(vim.o.lines * 0.8),
+        },
+      })
+    end
+  },
+
   -- LSP / 補完設定
   {
     "neovim/nvim-lspconfig",
